ba4dd6bf74ec6ff421c74f8a418cd51082c030a7
Willian Alves da Silva <willian_alves_df@hotmail.com>
2019-11-06 13:17:29 -0300

    ok

---
+++ CursoBlazor/Shared/Entidades/Filme.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoBlazor.Shared.Entidades
+{
+    public class Filme
+    {
+        public string Titulo { get; set; }
+        public DateTime Lancamento { get; set; }
+
+    }
+}
