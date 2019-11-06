2c7063c43d7c9937eb259bfeb7ff7ab45ba2e6d8
Willian Alves da Silva <willian_alves_df@hotmail.com>
2019-11-06 17:57:01 -0300

    IJSInterop

---
+++ CursoBlazor/Client/Helpers/IJSRuntimeExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoBlazor.Client.Helpers
+{
+    public static class IJSRuntimeExtensionMethods
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime js, string msg)
+        {
+            return await js.InvokeAsync<bool>("confirm", msg);
+        }
+    }
+}
---
+++ CursoBlazor/Client/Shared/FilmeComponents/ListarFilmes.razor.cs
@@ -0,0 +1,47 @@
+﻿using CursoBlazor.Client.Helpers;
+using CursoBlazor.Shared.Entidades;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoBlazor.Client.Shared.FilmeComponents
+{
+    public partial class ListarFilmes
+    {
+        [Inject] public IJSRuntime js { get; set; }
+        [Parameter] public List<Filme> Filmes { get; set; }
+        [Parameter] public bool MostrarBotao { get; set; } = false;
+
+        protected override void OnInitialized()
+        {
+            Console.WriteLine($"OnInitialized - Qtde. de Filmes: {Filmes.Count()}");
+        }
+        protected override void OnParametersSet()
+        {
+            Console.WriteLine($"OnParametersSet - Qtde. de Filmes: {Filmes.Count()}");
+        }
+        protected override void OnAfterRender(bool firstRender)
+        {
+            Console.WriteLine($"OnAfterRender: Executado pela 1ª vez? {firstRender}");
+        }
+        protected override bool ShouldRender()
+        {
+            Console.WriteLine($"ShouldRender");
+            return true;
+        }
+        async Task ExcluirFilme(Filme filme)
+        {
+            var confirma = await js.Confirm($"Deseja Excluir o Filme {filme.Titulo}?");
+
+            if (confirma)
+            {
+                Console.WriteLine($"Removendo FIlme: {filme.Titulo}");
+                Filmes.Remove(filme);
+
+            }
+        }
+    }
+}
