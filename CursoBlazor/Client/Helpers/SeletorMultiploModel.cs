53f65bb7a94dccc61ac203d6cc9e7ec066470423
Willian Alves da Silva <willian_alves_df@hotmail.com>
2019-11-26 17:21:56 -0300

    seletor multiplo

---
+++ CursoBlazor/Client/Helpers/SeletorMultiploModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoBlazor.Client.Helpers
+{
+    public struct SeletorMultiploModel
+    {
+        public SeletorMultiploModel(string chave, string valor)
+        {
+            Chave = chave;
+            Valor = valor;
+        }
+
+        public string Chave { get; set; }
+        public string Valor { get; set; }
+    }
+}
--- CursoBlazor/Shared/Entidades/CategoriaFilme.cs
+++ CursoBlazor/Shared/Entidades/CategoriaFilme.cs
@@ -6,7 +6,9 @@ namespace CursoBlazor.Shared.Entidades
 {
     public class CategoriaFilme
     {
+        public Filme Filme { get; set; }
         public int FilmeId { get; set; }
+        public Categoria Categoria { get; set; }
         public int CategoriaId { get; set; }
     }
 }
